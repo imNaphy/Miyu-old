93e7182e9c8befd63e62d4559b1cce1b785efddd
naphy <tvetcu.stefanos@gmail.com>
2021-08-17 06:23:37 +0300

    Update avatar.js

--- commands/Info/avatar.js
+++ commands/Info/avatar.js
@@ -5,11 +5,12 @@ module.exports = {
     run: async (bot, message, args) => {
         if (!message.guild.me.permissions.has('SEND_MESSAGES')) return;
         if (!args[0]) return message.channel.send(message.member.user.displayAvatarURL() + '?size=1024');
-        let member = message.mentions.members.first();
-        /*if (!member) {
-            const msg = args.join(' ');
-            member = bot.users.cache.find(user => user.username, msg).;
-        }*/
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        try {
+            await message.channel.send(member);
+        } catch (error) {
+            console.error(error);
+        }
         if(!member) return message.channel.send('ERROR: Wrong argument! Please mention someone!');
         return message.channel.send(member.user.displayAvatarURL() + '?size=1024');
     }
