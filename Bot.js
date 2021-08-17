78526028c1c70e114ea9ca3d39d0d9e54c522aff
naphy <tvetcu.stefanos@gmail.com>
2021-08-17 05:56:35 +0300

    v12 -> v13 ( I )

--- Bot.js
+++ Bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
 
-const bot = new Discord.Client({ allowedMentions: { parse: ['users', 'roles'], repliedUser: true } });
+const bot = new Discord.Client({ intents: 32767 });
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
