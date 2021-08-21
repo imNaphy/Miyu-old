194545b3d1abbf9ea157698539fd4415f74d40a2
naphy <tvetcu.stefanos@gmail.com>
2021-08-21 16:53:12 +0300

    Update osu.js

--- commands/Games/osu.js
+++ commands/Games/osu.js
@@ -16,32 +16,55 @@ module.exports = {
         try {
             const user = args.join(' ');
             await osuAPI.getUser({ u: user }).then(u => {
+                //Ranking
                 let rank;
                 let rankCountry = '';
                 if (u.pp.raw === '0') {
                     rank = '-';
                 }
                 else {
-                    rank = `#${u.pp.rank}`;
-                    rankCountry = `#${u.pp.countryRank}`
+                    rank = u.pp.rank;
+                    rank = '#' + rank.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    rankCountry = u.pp.countryRank;
+                    rankCountry = '#' + rankCountry.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                 }
+                //PP
+                let pp = u.pp.raw;
+                pp = pp.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                //Ranked Score
+                let rankedScore = u.scores.ranked;
+                rankedScore = rankedScore.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                //Total Score
+                let totalScore = u.scores.total;
+                totalScore = totalScore.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                //Level
                 const level = u.level.slice(0, -4) + ` + ${u.level.slice(-3, -1)}%`;
+                //Accuracy
                 let accuracy;
                 if (u.accuracy === '100') accuracy = '100.00%';
                 else accuracy = u.accuracy.slice(0, 5) + '%';
-                var time = (u.secondsPlayed / 60 / 60).toString();
-                time = time.split('.')[0];
+                //Time Played
+                let time = (u.secondsPlayed / 60 / 60).toString();
+                time = time.split('.')[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                //Play Count
+                let playCount = u.counts.plays;
+                playCount = playCount.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+                //Pune steagul utilizatorului in loc de author photo.
+
                 const embed = new Discord.MessageEmbed()
                 .setAuthor(`osu! Standard Profile for ${u.name}`, `http://s.ppy.sh/a/${u.id}`)
                 .setThumbnail(`http://s.ppy.sh/a/${u.id}`)
                 .setColor('#2F3136')
                 .addField('› Bancho Rank', `${rank} (${u.country}${rankCountry})`, true)
                 .addField('› Level', level, true)
-                .addField('› PP', u.pp.raw, true)
+                .addField('› PP', pp, true)
                 .addField('› Accuracy', accuracy, true)
-                .addField('› Playcount', u.counts.plays, true)
+                .addField('› Play Count', playCount, true)
                 .addField('› Time Played', `${time} hours`, true)
-                .addField('› Ranks', `<:SSH:878549683931324456>\`${u.counts.SSH}\`<:SS:878549683746775061>\`${u.counts.SS}\`<:SH:878549683679678474>\`${u.counts.SH}\`<:S_:878549683671269376>\`${u.counts.S}\`<:A_:878549683759382560>\`${u.counts.A}\``, true)
+                .addField('› Ranked Score', rankedScore, true)
+                .addField('› Total Score', totalScore, true)
+                .addField('› Ranks', `<:SSH:878549683931324456>\`${u.counts.SSH}\`<:SS:878549683746775061>\`${u.counts.SS}\`<:SH:878549683679678474>\`${u.counts.SH}\`<:S_:878549683671269376>\`${u.counts.S}\`<:A_:878549683759382560>\`${u.counts.A}\``, false)
                 .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
                 message.channel.send({embeds: [embed]});
             });
