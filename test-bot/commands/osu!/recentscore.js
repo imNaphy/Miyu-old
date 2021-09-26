80408263004ed71271e7eaeaaca580a692087f8d
naphy <tvetcu.stefanos@gmail.com>
2021-09-26 19:17:14 +0300

    Încercare de recentscore

--- test-bot/commands/osu!/recentscore.js
+++ test-bot/commands/osu!/recentscore.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
-const osu = require('node-osu');
-const ojsama = require('ojsama');
+
+var readline = require("readline");
+var osu = require('ojsama');
+/*const osu = require('node-osu');
 const osuAPI = new osu.Api(config.osuAPI, {
     notFoundAsError: true,
     completeScores: true,
@@ -68,7 +70,7 @@ function osuMods(bit) {
         finalMods += mods[i - 1];
     }
     return finalMods;
-}
+}*/
 
 module.exports = {
     name: 'recentscore',
@@ -79,6 +81,7 @@ module.exports = {
         try {
             const user = args.join(' ');
             //let username, userId, beatmapId, modsList, mods, title, version, beatmapSetId;
+            /*
             await osuAPI.getUser({ u: user }).then(player => {
                 username = player.name;
                 userId = player.id;
@@ -103,6 +106,68 @@ module.exports = {
             .setColor('#2F3136')
             .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
             message.channel.send({ content: `**Recent osu! Standard play for ${username}:**`, embeds: [embed]});
+            */
+            
+            
+            var mods = osu.modbits.none;
+            var acc_percent;
+            var combo;
+            var nmiss;
+            
+            // get mods, acc, combo, misses from command line arguments
+            // format: +HDDT 95% 300x 1m
+            var argv = process.argv;
+            
+            for (var i = 2; i < argv.length; ++i)
+            {
+              if (argv[i].startsWith("+")) {
+                mods = osu.modbits.from_string(argv[i].slice(1) || "");
+              }
+            
+              else if (argv[i].endsWith("%")) {
+                acc_percent = parseFloat(argv[i]);
+              }
+            
+              else if (argv[i].endsWith("x")) {
+                combo = parseInt(argv[i]);
+              }
+            
+              else if (argv[i].endsWith("m")) {
+                nmiss = parseInt(argv[i]);
+              }
+            }
+            
+            var parser = new osu.parser();
+            readline.createInterface({
+              input: process.stdin, terminal: false
+            })
+            .on("line", parser.feed_line.bind(parser))
+            .on("close", function() {
+              var map = parser.map;
+              console.log(map.toString());
+            
+              if (mods) {
+                console.log("+" + osu.modbits.string(mods));
+              }
+            
+              var stars = new osu.diff().calc({map: map, mods: mods});
+              console.log(stars.toString());
+            
+              var pp = osu.ppv2({
+                stars: stars,
+                combo: combo,
+                nmiss: nmiss,
+                acc_percent: acc_percent,
+              });
+            
+              var max_combo = map.max_combo();
+              combo = combo || max_combo;
+            
+              console.log(pp.computed_accuracy.toString());
+              console.log(combo + "/" + max_combo + "x");
+            
+              console.log(pp.toString());
+            });
         } catch (error) {
             console.error(error);
             return message.channel.send('ERROR: User not found!');
