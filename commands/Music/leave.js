2a092b2a8a3060d97ad1d0bd6f3d6494ccee17aa
naphy <tvetcu.stefanos@gmail.com>
2021-10-09 23:17:41 +0300

    Update leave.js

--- commands/Music/leave.js
+++ commands/Music/leave.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const DiscordVoice = require('@discordjs/voice');
 
 module.exports = {
     name: 'leave',
@@ -6,7 +7,7 @@ module.exports = {
     run: async (bot, message, args) => {
         if (!message.guild.me.permissions.has('SEND_MESSAGES')) return;
         if (!message.member.voice.channel) return message.channel.send('ERROR: You have to be in a voice channel first!');
-        connection = await message.member.voice.channel.leave();
+        DiscordVoice.getVoiceConnection(message.member.voice.channel.guild.id).disconnect();
         return;
     }
 };
