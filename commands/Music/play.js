c6b5d7ef9988a95032a59fe55c626bfe7bdc6fc5
naphy <tvetcu.stefanos@gmail.com>
2021-10-09 23:33:50 +0300

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -3,12 +3,11 @@ const ytdl = require('ytdl-core');
 
 module.exports = {
     name: 'play',
-    aliases: ['p', 'yt', 'youtube'],
+    aliases: ['p'],
     run: async (bot, message, args) => {
         if (!message.guild.me.permissions.has('SEND_MESSAGES')) return;
         if (!message.member.voice.channel) return message.channel.send('ERROR: You have to be in a voice channel first!');
-        if (!message.guild.voice.channel) connection = await message.member.voice.channel.join();
-
+        
         
     }
 };
