adac47afa2735d56d499c9f5d287c5c5e6be5571
naphy <tvetcu.stefanos@gmail.com>
2021-10-10 23:25:10 +0300

    Comandă de play semi-finalizată

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const DiscordVoice = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
+const YouTube = require('simple-youtube-api');
 
 module.exports = {
     name: 'play',
@@ -11,6 +12,7 @@ module.exports = {
         if (!permissions.has('CONNECT')) return message.channel.send('ERROR: I can\'t conenct to the voice channel!');
         if (!permissions.has('SPEAK')) return message.channel.send('ERROR: I can\'t talk in this voice channel!');
         if (!message.member.voice.channel) return message.channel.send('ERROR: You have to be in a voice channel first!');
+        if (message.guild.me.voice.channel !== message.member.voice.channel && message.guild.me.voice.channel && message.member.voice.channel) return message.channel.send('ERROR: I am in a different voice channel!');
         if (!message.guild.me.voice.channel) {
             try {
                 await DiscordVoice.joinVoiceChannel({ channelId: message.member.voice.channel.id, guildId: message.guild.id, adapterCreator: message.guild.voiceAdapterCreator });
@@ -23,7 +25,42 @@ module.exports = {
         const url = arguments[1] ? arguments[1].replace(/<(.+)>/g, '$1') : '';
         //const serverQueue = queue.get(message.guild.id);
         console.log(`${searchString} \n${url}`);
-        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) message.channel.send('youtube playlist');
-        
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+            const playlist = await YouTube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+            for (const video of Object.values(videos)) {
+                const video2 = await YouTube.getVideoById(video.id);
+                //await handleVideo(video2, message, voiceChannel,)
+            }
+            return message.channel.send(`The **${playlist.title}** playlist has been added to the queue!`);
+        } else if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/watch(.*)$/)) {
+            try {
+                const video = await YouTube.getVideo(url);
+                //return handleVideo(video, message, voiceChannel);
+            } catch (error) {
+                return message.channel.send('I couldn\'t find any video based on the URL provided!');
+            }
+        } else {
+            try {
+                var videos = await YouTube.searchVideos(searchString, 10);
+                let index = 0;
+                message.channel.send(`**Select a video:** \n${videos.map(video2 => `** ${++index}.** ${video2.title}`).join(`\n`)} \n\nPlease provide a value to select one of the search results from 1 to 10.\nIf no value is provided within 30 seconds, it will be cancelled.`).then(msg => {msg.delete(30000)});
+                try {
+                    var response = await message.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11 , {
+                        maxMatches: 1,
+                        time: 30000,
+                        errors: ['time']
+                    });
+                } catch (error) {
+                    console.error(error);
+                    return message.channel.send('No or invalid value entered, cancelling video selection.');
+                }
+                const videoIndex = parseInt(response.first().content);
+                var video = await YouTube.getVideoByID(videos[videoIndex - 1].id);
+                //return handleVideo(video, message, voiceChannel);
+            } catch (error) {
+                return message.channel.send('I couldn\'t obtain any search results.');
+            }
+        }
     }
 };
