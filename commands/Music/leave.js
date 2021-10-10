348faffc54ac4943c99ae568b7ee0c28a7dfa25c
naphy <tvetcu.stefanos@gmail.com>
2021-10-10 13:42:41 +0300

    Comandă de leave finalizată pentru ultima dată cum trebuie

--- commands/Music/leave.js
+++ commands/Music/leave.js
@@ -11,9 +11,12 @@ module.exports = {
         try {
             await DiscordVoice.getVoiceConnection(message.member.voice.channel.guild.id).destroy();
         } catch (error) {
-            await DiscordVoice.joinVoiceChannel({ channelId: message.member.voice.channel.id, guildId: message.guild.id, adapterCreator: message.guild.voiceAdapterCreator });
-            await DiscordVoice.getVoiceConnection(message.member.voice.channel.guild.id).destroy();
-            return;
+            try {
+                await DiscordVoice.joinVoiceChannel({ channelId: message.member.voice.channel.id, guildId: message.guild.id, adapterCreator: message.guild.voiceAdapterCreator });
+                await DiscordVoice.getVoiceConnection(message.member.voice.channel.guild.id).destroy();
+            } catch (error) {
+                console.error(error);
+            }
         }
         return;
     }
